23e1f108c57c7b1109e3dca46a0013629a394dca
caderthewiz <caderthewiz@outlook.com>
2026-09-07 16:52:07 -0500

    Added vercel adapter

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	},
+
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries.
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+	}
+};
+
+export default config;
